908de5507ae4fa4f379418583b24cbdefafacd0b
manish <manishmaharzan@gmail.com>
2016-09-23 09:54:37 +0545

    Initial commit

---
+++ app/src/main/java/com/example/manis/blogging/BlogListActivity.java
@@ -0,0 +1,13 @@
+package com.example.manis.blogging;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlogListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_list);
+    }
+}
